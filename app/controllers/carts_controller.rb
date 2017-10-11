5885b62942b95074f69231a323503d85347f24da
magnus-thor <magnus.thor.ar@gmail.com>
2017-10-11 21:03:32 +0200

    moved duplicate code to a method

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -4,7 +4,7 @@ class CartsController < ApplicationController
 
   def add
     dish = Dish.find(params[:id])
-    if /(\D+)/.match(params[:amount]).nil?
+    if an_integer?(params[:amount])
       @cart.add(dish, dish.price, params[:amount].to_i)
       flash[:notice] = "#{dish.name} added to cart: #{params[:amount]}"
     else
@@ -16,7 +16,7 @@ class CartsController < ApplicationController
 
   def remove_item
     dish = Dish.find(params[:dish_id])
-    if /(\D+)/.match(params[:remove_amount]).nil?
+    if an_integer?(params[:remove_amount])
       @cart.remove(dish, params[:remove_amount].to_i)
       flash[:notice] = "#{params[:remove_amount]} #{dish.name} was removed from your cart"
     else
@@ -27,5 +27,9 @@ class CartsController < ApplicationController
 
   def show
     @cart = Cart.find(session[:cart_id])
+  end
+
+  def an_integer?(item)
+    /(\D+)/.match(item).nil?
   end
 end
