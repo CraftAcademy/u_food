5201b15e5d10d9637ad09a5f75c88da7a9ad0953
magnus-thor <magnus.thor.ar@gmail.com>
2017-10-14 09:19:19 +0200

    edited params to be used in active admin having problems with creating user inside active admin kepp getting error: raise Formtastic::UnknownInputError

--- app/admin/cart_item.rb
+++ app/admin/cart_item.rb
@@ -2,7 +2,7 @@ ActiveAdmin.register CartItem do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-# permit_params :list, :of, :attributes, :on, :model
+permit_params :owner_id, :owner_type, :quantity, :item_id, :item_type, :price_cents, :price_currency
 #
 # or
 #
