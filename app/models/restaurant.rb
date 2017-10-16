e66ba6ff2657d515d2a3b5bff6847949c7d719f5
Amanda <29342045+Sajaas@users.noreply.github.com>
2017-10-16 17:26:58 +0200

    testing stuff with Thomas

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,11 +1,14 @@
 class Restaurant < ApplicationRecord
   belongs_to :restaurant_category
-  validates :address, presence: true
-  geocoded_by :address
-  after_validation :geocode
   has_many :menus, dependent: :destroy
   has_many :dishes, through: :menus
   has_many :dish_categories, through: :dishes
+
+  geocoded_by :address
+
+  validates :address, presence: true
+  after_validation :geocode
+
   def self.for_markers
     all.to_a.map(&:serializable_hash).map do |restaurant|
       {name: restaurant['name'],
@@ -13,4 +16,5 @@ class Restaurant < ApplicationRecord
        longitude: restaurant['longitude']}
     end
   end
+
 end
