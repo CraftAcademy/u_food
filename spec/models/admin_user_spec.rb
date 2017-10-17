d3c05402cc13f8b5b49842e4a23ea8d395dc8801
magnus-thor <magnus.thor.ar@gmail.com>
2017-10-17 10:58:54 +0200

    added to rspec testing of admin user abilities

--- spec/models/admin_user_spec.rb
+++ spec/models/admin_user_spec.rb
@@ -13,4 +13,34 @@ RSpec.describe AdminUser, type: :model do
     it {is_expected.to have_db_column :encrypted_password}
     it {is_expected.to have_db_column :super_admin}
   end
+
+  describe 'Admin user' do
+    describe 'abilities' do
+      let(:admin){nil}
+
+      context 'when is an super admin' do
+        let(:admin){ FactoryGirl.create(:admin_user) }
+        subject(:ability){ Ability.new(admin) }
+
+        it 'can manage all' do
+          assert ability.can?(:manage, :all)
+        end      end
+
+
+      context 'when is an restaurant owner' do
+        let(:admin){ FactoryGirl.create(:admin_user, super_admin: false) }
+        subject(:ability){ Ability.new(admin) }
+
+        it 'can read all' do
+          assert ability.can?(:read, :all)
+        end
+
+        it 'can not manage all' do
+          assert ability.cannot?(:manage, :all)
+        end
+      end
+
+
+    end
+  end
 end
