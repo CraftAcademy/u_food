fd99cac05cc04dd03378532067498d6a8d1a4608
basil <basil.mawejje@gmail.com>
2017-11-02 11:22:59 +0200

    Add routes for user api endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1, defaults: { format: :json } do
       resources :restaurants, only: [:index]
+      mount_devise_token_auth_for 'User', at: 'auth'
     end
   end
 
